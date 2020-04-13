3792be76646817db63ba8725cd68fa5d72288228
pirouz sourati zanjani <pirouz@pirouzs-MacBook-Pro.local>
2020-04-13 13:17:13 +0200

    initial commit

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -4,9 +4,11 @@ import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses=HelloController.class)
 public class Application {
 
 	public static void main(String[] args) {
