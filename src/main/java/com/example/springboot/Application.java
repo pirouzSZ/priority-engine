6cad5ecdf36605e4e1839c2206d9b658d06b3e95
pirouz sourati zanjani <pirouz@pirouzs-MacBook-Pro.local>
2020-04-13 15:43:34 +0200

    commit

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -1,17 +1,16 @@
 package com.example.springboot;
 
 import com.example.springboot.controller.UploadController;
+import com.example.springboot.domains.Case;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-//@ComponentScan(basePackageClasses= UploadController.class)
-@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
+@ComponentScan(basePackageClasses= UploadController.class)
 public class Application {
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
