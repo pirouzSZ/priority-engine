fdbab7ed26f03829cbdf907c92585ff0bf053e77
pirouz sourati zanjani <pirouz@pirouzs-MacBook-Pro.local>
2020-04-13 15:46:45 +0200

    commit

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses= UploadController.class)
+@ComponentScan({"com.example.springboot.controller"})
 public class Application {
 
 	public static void main(String[] args) {
