df45e8f8a102e9d38f5fe571be04df82ed4893c4
pirouz sourati zanjani <pirouz@pirouzs-MacBook-Pro.local>
2020-04-13 15:39:16 +0200

    commit

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -2,12 +2,15 @@ package com.example.springboot;
 
 import com.example.springboot.controller.UploadController;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses= UploadController.class)
+//@ComponentScan(basePackageClasses= UploadController.class)
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- src/main/java/com/example/springboot/controller/UploadController.java
+++ src/main/java/com/example/springboot/controller/UploadController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -37,7 +35,7 @@ public class UploadController {
             try (Reader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
 
                 // create csv bean reader
-                CsvToBean<Case> csvToBean = new CsvToBeanBuilder(reader)
+                CsvToBean csvToBean = new CsvToBeanBuilder(reader)
                         .withType(Case.class)
                         .withIgnoreLeadingWhiteSpace(true)
                         .build();
